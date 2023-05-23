e041e36ebd29f0374a3fe1bce4010187d77c550e
RJ Nowling <130711295+rnowling-memphis@users.noreply.github.com>
2023-05-23 07:45:47 -0500

    Fix gradle wrapper (#28)
    * Fix gradle wrapper so it runs now
    
    * Set source compatibility to version 11 since many projects still require Java 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ dependencies {
 }
 
 configure<JavaPluginExtension> {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks {
